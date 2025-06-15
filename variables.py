b56d5278c57f31db78900b6e390d8543231247f6
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-06-15 16:42:18 +0545

    Basics of variables and operators

---
+++ variables.py
@@ -0,0 +1,10 @@
+string1= "This is basics of python"
+string2= " This is pathshala"
+#This is adding Strings
+print (string1+ string2)
+
+num1 =1
+num2 =2
+sum = num1+num2
+print(sum)
+print (f"This is sum {sum}")
