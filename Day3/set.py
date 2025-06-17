6c680fd57a6c56a8b0ef0b1399f34bba793bb4e0
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-06-17 16:37:48 +0545

    python

---
+++ Day3/set.py
@@ -0,0 +1,29 @@
+#set in python
+set1= {"hello", "world", "python", 2, 1, "hello"}
+set2 = {"hello", 3, 4, "world",1}
+#print(set1)  # Output: {'world', 'python', 2, 'hello'}
+
+
+
+# #adding data to set
+# set1.add(551)# to insert use add
+# #print(set1)
+
+# #union in set
+# print(set1.union(set2))
+
+
+# #intersection
+# print(set1.intersection(set2))
+
+
+
+# #difference in set
+# print(set1.difference(set2))
+# print("Difference: ", set1 - set2)
+
+
+#symmetric difference
+print(set1.symmetric_difference(set2))
+
+
--- operators.py
+++ operators.py
@@ -45,3 +45,8 @@ print(num1)
 
 num1 **= 2
 print(num1)
+
+
+
+
+   
