7c6a248f9f8fe7951b50520445af27607b5cd9bd
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-06-19 17:02:27 +0545

    OOP

---
+++ OOP_calculators.py
@@ -0,0 +1,24 @@
+# calculators using OOP in python
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+    def __call__(self, operation):
+        num1 = self.num1
+        num2 = self.num2
+        if operation == 'add':
+            return f' The sum is {num1 + num2}'
+        elif operation == 'subtract':
+            return f'The difference is {num1 - num2}'
+        elif operation == 'multiply':
+            return f'The product is {num1 * num2}'
+        elif operation == 'divide':
+            return f' The quotient is {num1 / num2}'
+        else:
+            return 'Invalid operation'
+        
+calc = Calculator(10,20)
+result = calc('add')
+print(result)  # Output: The sum is 30
