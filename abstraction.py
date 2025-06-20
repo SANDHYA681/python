4b0de95fb5925c83f14dc23eb95e79ea3f4f3a2b
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-06-20 16:14:28 +0545

    abstraction

--- abstraction.py
+++ abstraction.py
@@ -1,24 +1,29 @@
-#Abstraction in poython
+#Abstraction in python
+
 from abc import ABC, abstractmethod
 
-
 class Wallets(ABC):
-    
- @abstractmethod
- def pay(self):
-        print("Test")
+    @abstractmethod
+    def pay(self,amount):
         pass
+
+class Paypal(Wallets):
+    def pay(self,amount):
+        print(f"\nPaypal payment successful with amount {amount} via paypal")
+
+class Khalti(Wallets):
+    def pay(self,amount):
+        print(f"\nKhalti payment successful with amount {amount} via khalti")
+
+def payment(wallet: Wallets, amount):
+    wallet.pay(amount)
     
- class Paypal(Wallets):
-    def pay(self):
-        print("Paypal payment")
-        
-    def displayInfo():
-         Wallets.pay()
-         
-    
-    obj = Paypal
-    obj.displayInfo()
-        
-    
- 
+wallet = input("Enter wallet name: ")
+amount = float(input("Enter amount to be paid: "))
+
+if wallet.lower() == "paypal":
+    payment(Paypal(), amount)
+elif wallet.lower() == "khalti":
+    payment(Khalti(), amount)
+else:
+    print("\nInvalid wallet name")
