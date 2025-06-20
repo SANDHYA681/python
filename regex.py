270b3170e53d5c6f29af658f61567d9a89bb1f29
SANDHYA681 <sandhyatimsina438@gmail.com>
2025-06-20 16:40:37 +0545

    regex code

--- regex.py
+++ regex.py
@@ -2,25 +2,39 @@
 
 import re
 
-# pattern for regex
-pattern = r'[a-z]'
-# string to search
-string = "Test"
 
-# matching the pattern 
-match = re.search(pattern, string)
-# print the match
-print(match)
+#Pattern for regex
+pattern = "test"
+string = "test string"
+
+# matching the pattern in the string
+if re.match(pattern, string):
+    print("Pattern found in the string")
+    
+    
+    #search
+if re.search(pattern, string):
+    print("Pattern found in the string")
+
+# # pattern for regex
+# pattern = r'[a-z]'
+# # string to search
+# string = "Test"
+
+# # matching the pattern 
+# match = re.search(pattern, string)
+# # print the match
+# print(match)
 
 
 
 
-#matching the pattern in string
-searching_string = re.search(pattern, string)
-# print the match
-print(searching_string)
+# #matching the pattern in string
+# searching_string = re.search(pattern, string)
+# # print the match
+# print(searching_string)
 
 
-match_string = re.match(pattern, string)
-# print the match
-print(match_string)
+# match_string = re.match(pattern, string)
+# # print the match
+# print(match_string)
